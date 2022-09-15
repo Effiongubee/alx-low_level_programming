cd40942b33b80463366bec2ff4926660e3ca60b4
Effiongubee <ephraimbros@gmail.com>
2022-09-15 13:30:04 -0700

    0-main.c, main.h

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifnder MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largeast[D_number(int, int, int);
+void print_remaining_days(int, int, int);
+int convert_day(int, int);
+
+#endif /* MAIN_H */
