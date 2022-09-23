56ba27836698fc90de3f337e241e918ac07b18c6
Effiongubee <ephraimbros@gmail.com>
2022-09-22 20:32:20 -0700

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,18 +9,17 @@
 void reverse_array(int *a, int n)
 
 {
-		int *p, I, aux, k;
-			p = a;
-				for (I = 1; I < n; i++)
-						{
-									p++;
-										}
-					for (k = 0; k < I / 2; k++)
-							{
-										aux = a[k];
-												a[k] = *p;
-														*p = aux;
-																p--;
-																	}
+	int *p, I, aux, k;
+	p = a;
+	for (I = 1; I < n; i++)
+	{
+		p++;
+	}
+	for (k = 0; k < I / 2; k++)
+	{
+		aux = a[k];
+		a[k] = *p;
+		*p = aux;
+		p--;
+	}
 }
-
