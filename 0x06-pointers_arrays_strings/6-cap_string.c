21f080e9fce92024845a9ae25308e023cd022272
Effiongubee <ephraimbros@gmail.com>
2022-09-22 20:46:23 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,19 +10,19 @@ char *cap_string(char *s)
 
 
 {
-		Int a = 0, I;
-			Int cspc = 13;
-				Char spc[] = {32, ‘\t’, ‘\n’, 44, 46, ‘!’, ‘?’, ‘”’, ‘(‘, ‘)’, ‘(‘, ‘)’};
-					While (s[a])
-							{
-										I = 0;
-												While (I < cspc)
-															{
-																			If ((a == 0 || s[a – 1] == spc[i]) && (s[a] >= 97 && s[a] <= 122))
-																								S[a] -= 32;
-																						I++;
-																								}
-														A++;
-															}
+	int a = 0, I;
+	int cspc = 13;
+	char spc[] = {32, ‘\t’, ‘\n’, 44, 46, '!', '?', '"', '(', ')', '{', '}'};
+	while (s[a])
+	{
+		i = 0;
+		while (I < cspc)
+		{
+			if ((a == 0 || s[a – 1] == spc[i]) && (s[a] >= 97 && s[a] <= 122))
+				s[a] -= 32;
+			i++;
+		}
+		a++;
+	}
+	return (s);
 }
-
