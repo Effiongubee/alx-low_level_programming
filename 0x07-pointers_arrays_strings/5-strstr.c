2eae79301e6c8a08eb0723cc1d159fa19c2f2001
Effiongubee <ephraimbros@gmail.com>
2022-09-26 22:16:13 -0700

    up 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -5,14 +5,15 @@
  * @haystack: string in which to check for needle
  * @needle: substring to find in haystack
  *
- * Return: pointer to begining of needle in haystack or NULL if no match
+ * Return: pointer to begining of needle in
+ * haystack or NULL if no match
  */
 
 char *_strstr(char *haystack, char *needle)
 
 {
 	unsigned int i = 0, j = 0;
-	
+
 	while (haystack[i])
 	{
 		while (needle[j] && (haystack[i] == needle[0]))
@@ -29,7 +30,6 @@ char *_strstr(char *haystack, char *needle)
 		}
 		else
 			return (haystack + i);
-
 	}
 	return (0);
 }
